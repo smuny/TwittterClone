dcd5fb980fb7f92003d589ba8ac590885e73b113
github username <github email address>
2019-06-19 12:49:13 -0400

    added relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_162809) do
+ActiveRecord::Schema.define(version: 2019_06_19_164059) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
